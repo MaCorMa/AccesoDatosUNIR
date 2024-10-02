c98cccac5cfd19303e2e61fdf7da81be28610399
MaCorMa <manuel.correcher@gmail.com>
2024-10-02 23:32:09 +0200

    lecturaJson

---
+++ lecturaJson/src/main/java/Entrada.java
@@ -0,0 +1,49 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+
+public class Entrada {
+
+    public static void main(String []args){
+
+        File file = new File("src/main/resources/usuario.txt");
+        //txt → JSON
+        BufferedReader bufferedReader = null;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuffer lecturaCompleta = new StringBuffer();
+            String linea = null;
+
+            while((linea= bufferedReader.readLine())!=null){
+                lecturaCompleta.append(linea);
+            }
+
+            System.out.println(lecturaCompleta.toString());
+            JSONObject usuario = new JSONObject(lecturaCompleta.toString());//pasa el contenido a string para poder crear el objeto JSON
+            System.out.println(usuario);
+            //Preguntar por clave y leer el dato asociado
+            String nombreUsuario = usuario.getString("nombre");
+            System.out.println(nombreUsuario);
+
+            //Preguntar por objeto y leer dato
+            Object apellidoUsuario = usuario.get("apellido");
+            JSONArray asignaturas = usuario.getJSONArray("asignaturas");
+            System.out.println(apellidoUsuario);
+            System.out.println(asignaturas);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error en el fichero");
+        } catch (IOException e) {
+            System.out.println("Error al leer");
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException | NullPointerException e) {
+                System.out.println("Error en el cerrado");
+            }
+        }
+
+    }
+}
